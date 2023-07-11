3358681d04249357cce942c53497bf345fb49aca
Sara Roseira <42982497+sararoseira@users.noreply.github.com>
2023-07-11 16:28:39 +0100

    added html, css, and js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
 "strict";
 
-let chosenCountry = document.querySelector("#countries");
+// let chosenCountry = document.querySelector("#countries");
 
-const showTimeZone = function (event) {
-  if (event.target.value.length > 0) {
-    let currentTime = moment()
-      .tz(event.target.value)
-      .format("dddd, MMMM, Do, YYYY h:mm A");
+// const showTimeZone = function (event) {
+//   if (event.target.value.length > 0) {
+//     let currentTime = moment()
+//       .tz(event.target.value)
+//       .format("dddd, MMMM, Do, YYYY h:mm A");
 
-    alert(`It is ${currentTime} in ${event.target.value}`);
-  }
+//     alert(`It is ${currentTime} in ${event.target.value}`);
+//   }
+// };
+
+// chosenCountry.addEventListener("change", showTimeZone);
+
+let country = document.querySelector("#lisbon");
+
+const cenas = function () {
+  let countryTime = moment().tz("Europe/Lisbon").format("H:m:ss");
+  console.log(countryTime);
+
+  country.innerHTML = `<h2>Lisbon</h2><h2>${countryTime}</h2>`;
 };
 
-chosenCountry.addEventListener("change", showTimeZone);
+setInterval(cenas, 1000);
